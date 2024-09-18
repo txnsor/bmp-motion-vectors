b2c02b1e4e458545d07ba9a6342a6e2293e99d94
Marc <mnb7940@rit.edu>
2024-09-17 20:04:30 -0400

    reoriented project

--- read_file.c
+++ read_file.c
@@ -6,7 +6,7 @@
 
 #include <stdio.h>
 #include <string.h>
-#define PNG_VALIDATION_SIZE 8
+#define BMP_VALIDATION_SIZE 8
 
 int main(int argc, char *argv[]) {
     if (argc <= 1) {return 0;}
@@ -24,17 +24,17 @@ int main(int argc, char *argv[]) {
     // validate that the file is a .bmp (WIP)
     char * bmp_signature;
 
-    char buffer[PNG_VALIDATION_SIZE + 1];
-    buffer[PNG_VALIDATION_SIZE] = '\0';
+    char buffer[BMP_VALIDATION_SIZE + 1];
+    buffer[BMP_VALIDATION_SIZE] = '\0';
 
-    size_t elements_read = fread(buffer, sizeof(char), PNG_VALIDATION_SIZE, file);
+    size_t elements_read = fread(buffer, sizeof(char), BMP_VALIDATION_SIZE, file);
 
-    for (int i = 0; i < PNG_VALIDATION_SIZE; i++) {
+    for (int i = 0; i < BMP_VALIDATION_SIZE; i++) {
         printf("buffer: %d\nvalid:%d\n", buffer[i], bmp_signature[i]);
     }
 
     if (strcmp(buffer, bmp_signature) != 0) {
-        printf("Error: Read %s (expected %s). File is not in .png format.\n", buffer, png_signature);
+        printf("Error: Read %s (expected %s). File is not in .BMP format.\n", buffer, bmp_signature);
         return 0;
     }
 
