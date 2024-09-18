c6d369d544e53560e6efd38b95e241a1ae7c31cc
Marc <mnb7940@rit.edu>
2024-09-17 20:01:39 -0400

    changed project goals and formatted WIP

--- read_file.c
+++ read_file.c
@@ -1,10 +1,11 @@
 /**
  * @author Marc Browning
- * @name PNG Reader
- * @brief Opens an image file.
+ * @name BMP Reader
+ * @brief Opens a .bmp image file.
  */
 
 #include <stdio.h>
+#include <string.h>
 #define PNG_VALIDATION_SIZE 8
 
 int main(int argc, char *argv[]) {
@@ -20,18 +21,23 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    // validate that the file is a .png
-    int png_signature[PNG_VALIDATION_SIZE] = {
-        137, 80, 78, 71, 13, 10, 26, 10
-    };
-    char buffer[PNG_VALIDATION_SIZE];
+    // validate that the file is a .bmp (WIP)
+    char * bmp_signature;
+
+    char buffer[PNG_VALIDATION_SIZE + 1];
+    buffer[PNG_VALIDATION_SIZE] = '\0';
+
     size_t elements_read = fread(buffer, sizeof(char), PNG_VALIDATION_SIZE, file);
+
     for (int i = 0; i < PNG_VALIDATION_SIZE; i++) {
-        if (buffer[i] != png_signature[i]) {
-            printf("Error: Byte %d read at position %d in file (expected %d). File is not in .png format.\n", buffer[i], i, png_signature[i]);
-            return 0;
-        }
+        printf("buffer: %d\nvalid:%d\n", buffer[i], bmp_signature[i]);
     }
+
+    if (strcmp(buffer, bmp_signature) != 0) {
+        printf("Error: Read %s (expected %s). File is not in .png format.\n", buffer, png_signature);
+        return 0;
+    }
+
     // close the file
     fclose(file);
     return 0;
